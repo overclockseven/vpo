9fd7a7d0f8b83d4c7f4201af81ba3f9bb2fc1142
Sasaki x Haise <92816853+overclockseven@users.noreply.github.com>
2021-12-10 00:35:27 +0700

    Add files via upload

---
+++ doansorandom/src/Main.java
@@ -0,0 +1,63 @@
+// Java Program to guess a Random Number Generation
+  
+import java.util.Random;
+import java.util.Scanner;
+  
+public class Main {
+public static void main(String[] args)
+{
+  
+// stores actual and guess number
+int answer, guess;
+  
+// maximum value is 100
+final int MAX = 100;
+  
+try (// takes input using scanner
+Scanner in = new Scanner(System.in)) {
+// Random instance
+Random rand = new Random();
+  
+boolean correct = false;
+  
+// correct answer
+answer = rand.nextInt(MAX) + 1;
+  
+// loop until the guess is correct
+while (!correct) {
+  
+System.out.println(
+"Guess a number between 1 and 100: ");
+  
+// guess value
+guess = in.nextInt();
+  
+// if guess is greater than actual
+if (guess > answer) {
+System.out.println("Too high, try again");
+}
+  
+// if guess is less than actual
+else if (guess < answer) {
+System.out.println("Too low, try again");
+}
+  
+// guess is equal to actual value
+else {
+  
+System.out.println(
+"Yes, you guessed the number.");
+  
+correct = true;
+}
+// guess is 
+else {
+  
+    System.out.println(
+    "Try again?");
+}
+}
+  
+System.exit(0);
+}
+}
